561c209cf3f590dc2ecfc0b10d646baa044627ee
bishalthapa <thapabishal21tech@gmail.com>
2024-01-23 17:13:04 +0545

    error checking

--- app/pages/project.tsx
+++ app/pages/project.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-// import ProjectsListsPage from "../components/projects/projectsListsPage";
+import ProjectsListsPage from "../components/projects/projectsListsPage";
 
 const ProjectPage = () => {
   return (
     <>
-      <div>{/* <ProjectsListsPage /> */}</div>
+      <div>
+        <ProjectsListsPage />
+      </div>
     </>
   );
 };
